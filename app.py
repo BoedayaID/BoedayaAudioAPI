f7c30f0430b9f29dc8f5bfdf2d997af2ef76219f
Aditya F <adityafahrezantara@gmail.com>
2021-05-29 10:08:26 +0700

    Added raw file processing

--- app.py
+++ app.py
@@ -15,27 +15,37 @@ def predict():
     # file = open(audio_file.filename, "rb")
     # values = {"file": (audio_file.filename, file, "audio/wav")}
 
+    file_name = "abc"
+    raw_data = request.get_data()
+    f = open(file_name, "wb")
+    f.write(raw_data)
+    f.close()
 
-    # # get file from POST request and save it
-    # # audio_file = request.files["file"]
+    # get file from POST request and save it
+    # audio_file = request.files["file"]
     # file_name = str(random.randint(0, 100000))
-    # values.save(file_name)
+    # audio_file.save(file_name)
 
     # # instantiate keyword spotting service singleton and get prediction
-    # kss = Keyword_Spotting_Service()
-    # predicted_keyword = kss.predict(file_name)
+    kss = Keyword_Spotting_Service()
+    predicted_keyword = kss.predict(file_name)
 
     # # we don't need the audio file any more - let's delete it!
-    # os.remove(file_name)
+    os.remove(file_name)
 
     # # send back result as a json file
-    # result = {"keyword": predicted_keyword}
-    # return jsonify(result)
+    result = {"keyword": predicted_keyword}
+    return jsonify(result)
     # return render_template("prediction.html", variable=str(audio_file.filename))
-    return "<h1>TEST</h1>"
+    # return "<h1>TEST</h1>"
 
-@app.route('/')
+@app.route('/', methods=["GET", "POST"])
 def index():
+    raw_data = request.get_data()
+    f = open("a.wav", "wb")
+    f.write(raw_data)
+    f.close()
+    # return raw_data
     return "<h1>Welcome to the server!</h1>"
 
 if __name__ == "__main__":
